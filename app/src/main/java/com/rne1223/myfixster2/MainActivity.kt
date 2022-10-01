02835a643a224ed06d9c8cdf05928bbfd951f42e
rne1223 <rne1223@gmail.com>
2022-10-01 00:57:21 -0700

    Added api key to project

--- app/src/main/java/com/rne1223/myfixster2/MainActivity.kt
+++ app/src/main/java/com/rne1223/myfixster2/MainActivity.kt
@@ -3,6 +3,9 @@ package com.rne1223.myfixster2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+private const val API_KEY = BuildConfig.API_KEY
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
