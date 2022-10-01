008a66ccf57a252ee20076a31d987d6d13a46402
rne1223 <rne1223@gmail.com>
2022-10-01 00:25:15 -0700

    starting project

---
+++ app/src/main/java/com/rne1223/myfixster2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rne1223.myfixster2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
