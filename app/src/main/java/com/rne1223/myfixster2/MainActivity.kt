b17f510131bebdfeeb1c90faa82ba8c30f35d6f0
rne1223 <rne1223@gmail.com>
2022-10-01 01:44:19 -0700

    Created recycler viewer in xml and binding it in MainActivity

--- app/src/main/java/com/rne1223/myfixster2/MainActivity.kt
+++ app/src/main/java/com/rne1223/myfixster2/MainActivity.kt
@@ -2,13 +2,24 @@ package com.rne1223.myfixster2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
 
 
-private const val API_KEY = BuildConfig.API_KEY
+//private const val API_KEY = BuildConfig.API_KEY
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var moviesRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the recycler viewer from xml
+        moviesRecyclerView = findViewById<RecyclerView>(R.id.moviesRecyclerView)
+
+        Log.v("MainActivity", "Testing")
+
     }
 }
